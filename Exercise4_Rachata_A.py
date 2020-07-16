bd9255ef6571f8cf8c15375c270dc17ecba69b15
Monkeysift69 <68386580+Monkeysift69@users.noreply.github.com>
2020-07-16 21:41:56 +0700

    Create Exercise4_Rachata_A.py

---
+++ Exercise4_Rachata_A.py
@@ -0,0 +1,9 @@
+FE = 38.9
+GB = 78
+ICS = 86.3
+CP = 97.5
+print ("--- Your Score ---")
+print ("Foundation English :", FE)
+print ("General Business :", GB)
+print ("Introduction to Computer Systems :", ICS)
+print ("Computer Programming : ", CP)
